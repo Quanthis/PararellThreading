6b593c6fffeb5abbd25dd046c106d2b54c429db9
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-23 19:38:20 +0100

    Oopsie, now code should be there

--- BackgroundWorker/Program.cs
+++ BackgroundWorker/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using static System.Console;
+using static System.Convert;
+using System.Threading;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace BackgroundWorker
 {
     class Program
     {
+        static void DoSomeWork(int threadNumber)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                WriteLine($"Here am I, thread {threadNumber}!");
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void NiceHowAboutReading()
+        {
+            WriteLine("Write something and I will do it again.");
+            string s = ReadLine();
+            WriteLine(s);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var options = new ParallelOptions();
+            Parallel.Invoke(() => DoSomeWork(1), () => NiceHowAboutReading());
+            ReadKey();
         }
     }
 }
